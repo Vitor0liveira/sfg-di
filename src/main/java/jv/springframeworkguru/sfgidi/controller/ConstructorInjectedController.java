7f647fd64ce3034e1b8519f975e3e68e7d517cd3
Vitor0liveira <vitoroliveira.ads@gmail.com>
2022-03-31 16:41:09 -0300

    Including another way to implement a Constructor Injected

---
+++ src/main/java/jv/springframeworkguru/sfgidi/controller/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package jv.springframeworkguru.sfgidi.controller;
+
+import jv.springframeworkguru.sfgidi.services.GreetingService;
+
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
